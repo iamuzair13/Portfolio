82886e090d43c29e8ce0f2b95f7f27001a557f94
iuzair13 <uzair.works3@gmail.com>
2025-07-28 14:58:27 +0500

    Added Mock data file and changed the title

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"1acszTY2P36mZTWrzZRAZno63Q/0av1Hlyu88GPiRBs=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
